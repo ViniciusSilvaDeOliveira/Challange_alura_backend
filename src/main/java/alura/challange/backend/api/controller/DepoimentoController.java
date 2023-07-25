753ee21d7d1c21ce83d68b2f334a07a0288d9d28
ViniciusSilvaDeOliveira <tiviniciusoliveira@gmail.com>
2023-07-25 09:49:41 -0300

    Subindo pasta controller

---
+++ src/main/java/alura/challange/backend/api/controller/DepoimentoController.java
@@ -0,0 +1,9 @@
+package alura.challange.backend.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/depoimentos")
+public class DepoimentoController {
+}
