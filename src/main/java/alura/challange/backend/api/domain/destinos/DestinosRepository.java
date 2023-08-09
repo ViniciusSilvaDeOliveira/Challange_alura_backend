ae27f001fec0416448fe44b2e39c129d0bbe7356
ViniciusSilvaDeOliveira <tiviniciusoliveira@gmail.com>
2023-08-09 08:17:21 -0300

    Atualizando a pesquisa do Destino

--- src/main/java/alura/challange/backend/api/domain/destinos/DestinosRepository.java
+++ src/main/java/alura/challange/backend/api/domain/destinos/DestinosRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 public interface DestinosRepository extends JpaRepository<Destinos, Long> {
 
     @Query(value = """
-            select * from Destinos d where d.nome = :nome
+            SELECT * FROM Destinos d WHERE LOWER(d.nome) LIKE LOWER(CONCAT('%', :nome, '%'))
             """, nativeQuery = true)
     Destinos getDestinoByNome(String nome);
+
+    //select * from Destinos d where d.nome = :nome
+
+
 }
