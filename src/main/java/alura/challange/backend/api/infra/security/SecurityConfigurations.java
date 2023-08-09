a06b6e80ab221954127e8aca7e14803a9a37954f
ViniciusSilvaDeOliveira <tiviniciusoliveira@gmail.com>
2023-08-09 13:43:17 -0300

    Realizando a tratativa de mais alguns erros que podem ocorrer na API

--- src/main/java/alura/challange/backend/api/infra/security/SecurityConfigurations.java
+++ src/main/java/alura/challange/backend/api/infra/security/SecurityConfigurations.java
@@ -23,12 +23,13 @@ public class SecurityConfigurations {
 
     @Bean //não vai bloquear as Url, vamos determinar como será o processo de autenticação
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                .anyRequest().authenticated()//qualquer outra URL tem que estar logada
-                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
